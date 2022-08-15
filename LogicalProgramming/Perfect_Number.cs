2a719fea9c1e25c32a8a0093b4cb4262aeb1103f
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 22:39:46 +0530

    [Tejaswini]Add perfect number

---
+++ LogicalProgramming/Perfect_Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Perfect_Number
+    {
+        public void Perfect()
+        {
+            Console.WriteLine("Enter the Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int i, sum = 0;
+            for (i = 1; i < Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == Number)
+            {
+                Console.WriteLine("Number is Perfect");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Perfect");
+            }
+        }
+    }
+}
+
