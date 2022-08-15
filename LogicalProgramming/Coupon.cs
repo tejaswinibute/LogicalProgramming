b36a912fe8d41687446b7be18c5bde06885a9a2a
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 22:58:02 +0530

    [Tejaswini]add coupon Number

---
+++ LogicalProgramming/Coupon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Coupon
+    {
+        public void Coupon_Number()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Console.WriteLine(finalString);
+        }
+    }
+}
