d1d26e8a2cdc5888f23adb23ee41f399707a56f9
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 22:33:57 +0530

    [Tejaswini Bute] Add Fibonacci series

---
+++ LogicalProgramming/Fibonacci_Series.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Fibonacci
+    {
+        public void Fibonacci_Series()
+        {
+            Console.WriteLine("Enter the Terms for Fibonacci Series");
+            int Terms = Convert.ToInt32(Console.ReadLine());
+            int First_Term = 0;
+            int Second_Term = 1;
+            int Third_Term = 0;
+            Console.WriteLine(First_Term);
+            Console.WriteLine(Second_Term);
+            while (Terms > 0)
+            {
+                Third_Term = First_Term + Second_Term;
+                Console.WriteLine(Third_Term);
+                First_Term = Second_Term;
+                Second_Term = Third_Term;
+                Terms--;
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Functional_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Functional Programming");
+            Console.WriteLine("Enter 1 for Fibonacci Series");
+            int Ans = Convert.ToInt32(Console.ReadLine());
+            switch (Ans)
+            {
+                case 1:
+                    Fibonacci FibonacciObj = new Fibonacci();
+                    FibonacciObj.Fibonacci_Series();
+                    break;
+                default:
+                    Console.WriteLine("Entered Value is Wrong");
+                    break;
+            }
+        }
+    }
+}
+
+ 
