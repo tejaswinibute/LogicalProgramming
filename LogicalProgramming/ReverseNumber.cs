fe2301ce5714a43ee861ce312295a600a759673d
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 22:51:19 +0530

    [Tejaswini] add Reverse Number

---
+++ LogicalProgramming/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.WriteLine("Enter the number to be reversed");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int Rev = 0;
+            int sum = 0;
+            while (Number != 0)
+            {
+                Rev = Number % 10;
+                sum = Rev + sum * 10;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse of the number " + Number + " is " + sum);
+        }
+    }
+}
