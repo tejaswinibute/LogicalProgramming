c02f59c6fb6c547950d12d993fdb9a37f1608cb0
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 23:03:31 +0530

    [Tejaswini] Add Stopwatch Program

---
+++ LogicalProgramming/Stopwatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LogicalProgramming
+{
+    public class StopWatch
+    {
+        public void Stop_Watch()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            //B: Run stuff you want timed
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+            string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+
+        }
+    }
+}
